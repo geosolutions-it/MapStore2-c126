243f8c8f81f0add54b41e16c544c6652a981d396
stefano bovio <bovio.stefano@gmail.com>
2019-07-18 10:46:42 +0200

    Add custom theme (#15)
    * add custom theme
    
    * update footer in manage account page
    
    * update spinner color of catalog search button
    
    * override default style of chart previews

---
+++ js/plugins/Footer.jsx
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+const PropTypes = require('prop-types');
+const { Grid, Row, Col } = require('react-bootstrap');
+
+class Footer extends React.Component {
+
+    static propTypes = {
+        address: PropTypes.string,
+        socialLinks: PropTypes.array
+    };
+
+    static defaultProps = {
+        address: 'Newmont Goldcorp |Minera Peñasquito| Carretera Mazapil Cedros KM 24 Mazapil, Zacatecas C.P. 98230| T +52 842 424 2700',
+        socialLinks: [
+            {
+                "src": "assets/img/iconfinder_facebook_3999035.png",
+                "href": "https://www.facebook.com/Goldcorp"
+            },
+            {
+                "src": "assets/img/iconfinder_twitter_3999029.png",
+                "href": "https://twitter.com/Goldcorp_Inc"
+            },
+            {
+                "src": "assets/img/iconfinder_linkedin_3999038.png",
+                "href": "https://ca.linkedin.com/company/goldcorp-inc"
+            }
+        ]
+    };
+
+    render() {
+        return (
+            <Grid>
+                <Row>
+                    <Col xs={12} className="text-center">
+                        {this.props.address}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} className="gc-social-links-container">
+                        <div>
+                            {this.props.socialLinks.map((link, idx) => (
+                                <div key={idx} className="gc-social-link">
+                                    <a
+                                        href={link.href}
+                                        target={link.target || '_blank'}>
+                                        <img src={link.src} />
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
+}
+
+module.exports = {
+    FooterPlugin: Footer
+};
