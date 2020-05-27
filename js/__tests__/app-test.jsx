27993890deaa2ffd4dc52a55b6ed37126a9caba1
Matteo V <matteo.velludini@geo-solutions.it>
2020-05-27 11:20:29 +0200

    Update mapstore submodule to 2020.01.xx (#22)
    * Update mapstore submodule to 2020.01.xx
    * remove geostory from config
    * add missing changes for the pom
    * remove check for i18n on travis command
    * Fixed tests config
    * fix travis command

---
+++ js/__tests__/app-test.jsx
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2020, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+
+import expect from 'expect';
+
+describe('fake test', () => {
+    it('is a fake test', () => {
+        expect(true).toBe(true);
+    });
+});
